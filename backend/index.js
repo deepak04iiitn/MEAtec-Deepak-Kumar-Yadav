92e4dc28483d8fef1d854ef44318ad616c71d3af
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-11-30 01:52:26 +0530

    serving frontend from backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Middlewares
 app.use(cors());
@@ -34,6 +36,14 @@ app.use((req, res) => {
     success: false,
     message: 'Route not found',
   });
+});
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+// Catch-all route to serve the SPA for any unmatched routes
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 // Error handling middleware 
